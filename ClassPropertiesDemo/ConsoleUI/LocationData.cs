cc0964674beba8fba056911eb12264d98fc1067a
Murat Kugic <m.kugic@gmail.com>
2019-02-19 19:35:47 +0100

    Added Class Properties demo

---
+++ ClassPropertiesDemo/ConsoleUI/LocationData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class LocationData
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        public string FullAddress
+        {
+            get
+            {
+                return $"Street: { StreetAddress }\nCity: { City }\nState: { State }\nZipCode: { ZipCode }";
+            }
+        }
+
+        public LocationData()
+        {
+
+        }
+
+        public LocationData(string streedAddress, string city, string state, string zipCode)
+        {
+            StreetAddress = streedAddress;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
+    }
+}
---
+++ ClassPropertiesDemo/ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocationData location = new LocationData("123 Main", "San Diego", "California", "GT-32523");
+
+            Console.WriteLine(location.FullAddress);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
